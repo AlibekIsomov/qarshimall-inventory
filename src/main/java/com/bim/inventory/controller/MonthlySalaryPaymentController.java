47b0c49ff71c6b42e47c11b427dda72caae62fa5
Alibek Isomov <99270031+AlibekIsomov@users.noreply.github.com>
2024-01-30 15:04:25 +0500

    Update MonthlySalaryPaymentController.java

--- src/main/java/com/bim/inventory/controller/MonthlySalaryPaymentController.java
+++ src/main/java/com/bim/inventory/controller/MonthlySalaryPaymentController.java
@@ -21,7 +21,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/monthlySalaryPayemnt")
+@RequestMapping("/api/monthlySalaryPayment")
 public class  MonthlySalaryPaymentController {
 
     @Autowired
